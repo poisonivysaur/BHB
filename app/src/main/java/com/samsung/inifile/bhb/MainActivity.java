9983e6900226cba43b79fb4b208c56d7ee585107
ivy <robinnico.purple@gmail.com>
2018-07-05 21:52:09 +0800

    added camera intent

--- app/src/main/java/com/samsung/inifile/bhb/MainActivity.java
+++ app/src/main/java/com/samsung/inifile/bhb/MainActivity.java
@@ -1,16 +1,21 @@
 package com.samsung.inifile.bhb;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
@@ -18,17 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.d("Item", item + "");
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     return true;
                 case R.id.navigation_feed:
                     return true;
                 case R.id.navigation_scoop:
+                    //dispatchTakePictureIntent();
+                    Intent intent = new Intent(MainActivity.this, PhotoDetailsActivity.class);
+                    startActivity(intent);
                     return true;
                 case R.id.navigation_rank:
                     return true;
@@ -51,5 +62,21 @@ public class MainActivity extends AppCompatActivity {
         HomeFragment home = new HomeFragment();
         fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.main_fragment, home).commit();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //mImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
