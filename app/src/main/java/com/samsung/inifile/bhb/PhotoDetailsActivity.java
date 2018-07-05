d5d4e9b931c9cee8bffc4dadee70acd933f45620
ivy <robinnico.purple@gmail.com>
2018-07-05 23:01:20 +0800

    added input elements

--- app/src/main/java/com/samsung/inifile/bhb/PhotoDetailsActivity.java
+++ app/src/main/java/com/samsung/inifile/bhb/PhotoDetailsActivity.java
@@ -1,23 +1,79 @@
 package com.samsung.inifile.bhb;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class PhotoDetailsActivity extends AppCompatActivity{
+
+    private EditText caption;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_details);
+
+        caption = (EditText) findViewById(R.id.caption_input);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.post_photo_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_save) {
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT);
+        }
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // TODO check if caption is empty or not
+        if(caption.getText().toString().length() > 0){
+            showDeleteConfirmationDialog();
+        }
+        else
+            super.onBackPressed();
+    }
+
+    /**
+     * Prompt the user to confirm that they want to discard draft.
+     */
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the postivie and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure to discard your changes?");
+        builder.setPositiveButton("Discard", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Discard" button, so delete the draft.
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the media.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
