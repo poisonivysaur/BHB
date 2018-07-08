a043dfd3b537ece79a80ce74c685d55227860543
Ico <enrico_zabayle@dlsu.edu.ph>
2018-07-08 15:43:17 +0800

    Added attributes

--- app/src/main/java/com/samsung/inifile/bhb/Post.java
+++ app/src/main/java/com/samsung/inifile/bhb/Post.java
@@ -4,8 +4,33 @@ import android.graphics.Bitmap;
 
 public class Post {
 
-    private String id, photourl, caption;
-    private Bitmap imaage;
+    private String id, photourl, caption, name, rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Bitmap getProfpic() {
+        return profpic;
+    }
+
+    public void setProfpic(Bitmap profpic) {
+        this.profpic = profpic;
+    }
+
+    private Bitmap imaage, profpic;
 
     public Post() {}
 
